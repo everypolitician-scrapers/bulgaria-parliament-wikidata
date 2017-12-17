45f8577003d0cceb3cc51a5e95e86fc55132e767
Tony Bowden <tony@tmtm.com>
2017-12-17 10:41:36 +0000

    Also fetch via SPARQL

--- scraper.rb
+++ scraper.rb
@@ -9,9 +9,11 @@ require 'wikidata/fetcher'
   'Категория:Народни представители в 42 Народно събрание',
   'Категория:Народни представители в 41 Народно събрание',
   'Категория:Народни представители в 40 Народно събрание',
-  'Категория:Народни представители в 39 Народно събрание',
+  'Категория:Народни представители в 39 Народно събрание'
 ]
-
 names = @pages.map { |c| WikiData::Category.new(c, 'bg').member_titles }.flatten.uniq
-EveryPolitician::Wikidata.scrape_wikidata(names: { bg: names })
 
+sparq = 'SELECT DISTINCT ?item WHERE { ?item p:P39/ps:P39 wd:Q18924508 }'
+ids = EveryPolitician::Wikidata.sparql(sparq)
+
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { bg: names })
